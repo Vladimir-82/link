edd64d3c096f4d001dc1157f0c9d0c2553e6892d
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-07-12 16:48:28 +0300

    init

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Link(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    link = models.CharField(max_length=1000)
+    shortlink = models.CharField(max_length=1000)
+
+
+    def __str__(self):
+        return self.shortlink
