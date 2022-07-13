42a2f6b214ab287a78f99e8e3f6a1d12e861b516
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-07-13 16:49:27 +0300

    hz

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, AddCreateView
+from .views import index, linker
 
 urlpatterns = [
-    path('', AddCreateView.as_view(), name='add'),
+    # path('', AddCreateView.as_view(), name='add'),
     path('main', index, name='main'),
+    path('', linker, name='linker'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,14 +6,32 @@ from .forms import AddForm
 
 
 
-
-
 def index(request):
-    result = Link.objects.all()
-    return render(request, 'app/index.html', {'result': result})
+    links = Link.objects.all()
+    return render(request, 'app/index.html', {'links': links})
 
 
-class AddCreateView(CreateView):
-    template_name = 'app/create.html'
-    form_class = AddForm
-    success_url = ''
+# class AddCreateView(CreateView):
+#     template_name = 'app/create.html'
+#     form_class = AddForm
+#     success_url = ''
+
+
+
+def linker(request):
+    print(request.POST)
+    if request.method == 'POST':
+        print('post here')
+        form = AddForm(request.POST)
+        if form.is_valid():
+            print('ffffffffffffffffff')
+            form.save()
+
+            return render(request, 'app/index.html')
+
+    else:
+        form = AddForm()
+        context = {'form': form}
+        print('get here')
+        
+        return render(request, 'app/create.html', context)
