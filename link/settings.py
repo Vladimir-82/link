c3c96a0b40d90099e660e8a5e9ad1229549c9860
vova <grun_gespenst@tut.by>
2022-07-16 16:46:20 +0300

    checked deploy

--- link/settings.py
+++ link/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-25!vqt&vy-ii89ugv#o)zpp31qv0h%i+8g_brm3zv9_q+o8%$2'
+# SECRET_KEY = 'django-insecure-25!vqt&vy-ii89ugv#o)zpp31qv0h%i+8g_brm3zv9_q+o8%$2'
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-25!vqt&vy-ii89ugv#o)zpp31qv0h%i+8g_brm3zv9_q+o8%$2')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
