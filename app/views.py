0c82bd1f7648c23e99a5e0b24d4a1e88788b3b73
vova <grun_gespenst@tut.by>
2022-07-16 15:55:46 +0300

    completed

--- app/views.py
+++ app/views.py
@@ -1,30 +1,30 @@
 import uuid
 
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.contrib import messages
 from django.contrib.auth import login, logout
 from .models import Link
 from .forms import AddForm, UserRegisterForm, UserLoginForm
 
 
-# @login_required
 def create(request):
-    if request.method == 'GET':
-        form = AddForm()
-        return render(request, 'app/create.html', {'form': form})
-    if request.method == 'POST':
-        form = AddForm(request.POST)
-        if form.is_valid():
-            form.save()
-        links = Link.objects.last()
-        hash = uuid.uuid3(uuid.NAMESPACE_DNS, links.link)
-        new_link = hash.__str__()[:8]
-        links.shortlink = ''.join(('https://', 'mydomen/', new_link))
-        links.save()
-
-        return render(request, 'app/create.html', {'links': links})
-
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            form = AddForm()
+            return render(request, 'app/create.html', {'form': form})
+        if request.method == 'POST':
+            form = AddForm(request.POST)
+            if form.is_valid():
+                form.save()
+            links = Link.objects.last()
+            hash = uuid.uuid3(uuid.NAMESPACE_DNS, links.link)
+            new_link = hash.__str__()[:8]
+            links.shortlink = ''.join(('https://', 'mydomen/', new_link))
+            links.save()
+            return render(request, 'app/create.html', {'links': links})
+    else:
+        raise PermissionDenied
 
 def register(request):
     if request.method == 'POST':
@@ -58,8 +58,11 @@ def user_logout(request):
     return redirect('login')
 
 
-@login_required
+
 def show(request):
-    current_user = request.user.id
-    links = Link.objects.filter(author=current_user)
-    return render(request, 'app/show.html', {"links": links})
+    if request.user.is_authenticated:
+        current_user = request.user.id
+        links = Link.objects.filter(author=current_user)
+        return render(request, 'app/show.html', {"links": links})
+    else:
+        raise PermissionDenied
--- link/urls.py
+++ link/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view()),
     path('',  include('app.urls')),
 ]
