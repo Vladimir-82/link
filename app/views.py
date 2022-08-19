4cfd133f8a49ee13badf19a66e3e979b3ac8b949
vladimir <grun_gespenst@tut.by>
2022-08-19 07:47:46 +0300

    debug_TRUE

--- app/views.py
+++ app/views.py
@@ -43,10 +43,10 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            # messages.success(request, Message.success_register)
+            messages.success(request, Message.success_register)
             return redirect('/')
-        # else:
-            # messages.error(request, Message.error_register)
+        else:
+            messages.error(request, Message.error_register)
     else:
         form = UserRegisterForm()
     return render(request, 'app/register.html', {"form": form})
--- link/settings.py
+++ link/settings.py
@@ -33,9 +33,9 @@ else:
     ALLOWED_HOSTS = []
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU:
-    DEBUG = True
-
+# if not IS_HEROKU:
+#     DEBUG = True
+DEBUG = True
 
 # Application definition
 
